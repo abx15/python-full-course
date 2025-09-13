47cc5e847a9c407e71ba036ca36d9cf7da7aa8aa
abx15 <arunkumarbind150720@gmail.com>
2025-09-13 08:07:24 +0530

    basic function

---
+++ 06_python/bsic.py
@@ -0,0 +1,16 @@
+# basic function
+
+def showCompanyName():
+    print("Welcome to ABC Corporation")
+
+showCompanyName()
+
+
+# function with parameter 
+
+def showEmployeeName(name):      # name is parameter
+    print('Employee Name:' , name)
+
+showEmployeeName('John Doe')       # 'John Doe' is argument
+
+
