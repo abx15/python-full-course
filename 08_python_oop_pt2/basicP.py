32c42d79cf71d03424372b2949d0ce578c025114
abx15 <arunkumarbind150720@gmail.com>
2025-09-17 14:07:04 +0530

    basic file

---
+++ 08_python_oop_pt2/basicP.py
@@ -0,0 +1,12 @@
+class Animal:
+    def sound(self):
+        print("Some Sound")
+
+class Dog:
+    def sound(self):
+        print("Bark Bark")  
+
+animalObj = Animal()
+animalObj.sound()
+dogObj = Dog()
+dogObj.sound()
